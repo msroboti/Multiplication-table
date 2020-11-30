1b5a162c32e36253102c55bae4f92327c19605e0
msroboti <74275009+msroboti@users.noreply.github.com>
2020-11-30 18:09:05 +0330

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+int main()
+{
+	//جدول ضرب با آرایه دوبل
+	int i[10][10];
+	int x, y;
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			i[x][y] = (x + 1)*(y + 1);
+			printf(" %3d", i[x][y]);
+		}
+		printf("\n");
+	}
+	getchar();
+}
